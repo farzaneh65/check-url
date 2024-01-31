52c478360a5d8a97081d16caa1f6b613a4e4e546
Farzaneh <farzaneh65@gmail.com>
2024-01-31 13:38:13 +0330

    change style-fix bugs

--- background.js
+++ background.js
@@ -4,82 +4,66 @@ let tabStatusMap = {};
 const tabPorts = {};
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
-  console.log("onActivated");
+  //onActivated
+
   const tabId = activeInfo.tabId;
-  console.log(tabId);
-  if (tabStatusMap.hasOwnProperty(tabId)) {
-    console.log("Use the stored status");
-    updateIcon(tabStatusMap[tabId]);
-  } else {
-    console.log("Use new status");
-    chrome.tabs.get(activeInfo.tabId, function (tab) {
-      // Check if the tab is fully loaded (tab.url exists) and has a valid URL
-      // if (tab && tab.url && tab.url.startsWith("http")) {
-      //   console.log("onActivate-IF");
-      //   const url = tab.url;
-      //   console.log("Page loaded:", url);
-      //   let cleanURL = getDomain(url);
-      //   //console.log(isIPAddress(cleanURL));
-      //   if (!isIPAddress(cleanURL)) {
-      //     let urlParts = cleanURL.split(".");
-      //     //console.log(urlParts.length);
-      //     if (urlParts.length > 2) {
-      //       cleanURL =
-      //         urlParts[urlParts.length - 2] +
-      //         "." +
-      //         urlParts[urlParts.length - 1];
-      //     }
-      //     //console.log(cleanURL);
-      //   }
-      //   //createWarningBanner();
-      //   //console.log(tabId);
-      //   // createWarningBanner(tabId);
-      //   if (cleanURL !== "new-tab-page") {
-      //     checkURL(cleanURL, tabId);
-      //   }
-      // }
-    });
-  }
+
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    const url = tab.url;
+
+    if (tabStatusMap.hasOwnProperty(tabId)) {
+      //Use the stored status
+
+      updateIcon(tabStatusMap[tabId], url, tabId);
+    } else {
+      //Use new status
+      //Check if the tab is fully loaded (tab.url exists) and has a valid URL
+      if (tab && tab.url && tab.url.startsWith("http")) {
+        //onActivate-IF
+        let cleanURL = getDomain(url);
+        if (!isIPAddress(cleanURL)) {
+          let urlParts = cleanURL.split(".");
+          if (urlParts.length > 2) {
+            cleanURL =
+              urlParts[urlParts.length - 2] +
+              "." +
+              urlParts[urlParts.length - 1];
+          }
+        }
+
+        checkURL(cleanURL, tabId);
+      } else {
+        iconPath = "icon/unknownIcon.png";
+        chrome.action.setIcon({
+          path: { 16: iconPath, 48: iconPath, 128: iconPath },
+        });
+      }
+    }
+  });
 });
 
 chrome.webNavigation.onCommitted.addListener(function (details) {
-  console.log("onCommitted");
-  console.log(tabStatusMap);
-  //console.log(details.frameId);
+  //onCommitted
   const transitionType = details.transitionType;
-  console.log("transition Type: ", transitionType);
+  const url = details.url;
+
   if (details.frameId === 0) {
-    console.log("fameId is 0");
     const tabId = details.tabId;
-    console.log("tabId", tabId);
-
+    // Check if the tab is loaded
     if (tabStatusMap.hasOwnProperty(tabId) && transitionType === "reload") {
-      console.log("Use the stored status");
-      console.log("array:", tabStatusMap[tabId]);
-      // Use the stored status
-      updateIcon(tabStatusMap[tabId]);
-    } else {
-      console.log("Use new status");
-      const url = details.url;
-      console.log("Page loaded:", url);
-      let cleanURL = getDomain(url);
+      //Use the stored status
 
-      //console.log(isIPAddress(cleanURL));
+      updateIcon(tabStatusMap[tabId], url, tabId);
+    } else {
+      //Use new status
+      let cleanURL = getDomain(url);
       if (!isIPAddress(cleanURL)) {
         let urlParts = cleanURL.split(".");
-
-        //console.log(urlParts.length);
         if (urlParts.length > 2) {
           cleanURL =
             urlParts[urlParts.length - 2] + "." + urlParts[urlParts.length - 1];
         }
-        //console.log(cleanURL);
       }
-
-      //createWarningBanner();
-      //console.log(tabId);
-      // createWarningBanner(tabId);
-
       if (cleanURL !== "new-tab-page") {
         checkURL(cleanURL, tabId);
       } else {
@@ -92,38 +76,9 @@ chrome.webNavigation.onCommitted.addListener(function (details) {
   }
 });
 
-// chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
-//   const tabId = details.tabId;
-
-//   console.log("Use new status-HistoryStateUpdate");
-//   const url = details.url;
-//   console.log("Page loaded:", url);
-//   let cleanURL = getDomain(url);
-
-//   //console.log(isIPAddress(cleanURL));
-//   if (!isIPAddress(cleanURL)) {
-//     let urlParts = cleanURL.split(".");
-
-//     //console.log(urlParts.length);
-//     if (urlParts.length > 2) {
-//       cleanURL =
-//         urlParts[urlParts.length - 2] + "." + urlParts[urlParts.length - 1];
-//     }
-//     //console.log(cleanURL);
-//   }
-
-//   //createWarningBanner();
-//   //console.log(tabId);
-//   // createWarningBanner(tabId);
-//   if (cleanURL !== "new-tab-page" && tabId) {
-//     checkURL(cleanURL, tabId);
-//   }
-// });
-
 chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
   // Remove the tab from the tabStatusMap when it is closed
   delete tabStatusMap[tabId];
-  //console.log(tabStatusMap);
 });
 
 function checkURL(url, tabId) {
@@ -132,12 +87,7 @@ function checkURL(url, tabId) {
     .then((whitelist) => {
       const whitelistedURLs = whitelist.split("\n").map((line) => line.trim());
       const isWhitelisted = whitelistedURLs.includes(url);
-      //console.log(isWhitelisted);
       tabStatusMap[tabId] = isWhitelisted;
-      // if (!isWhitelisted) {
-      //   createWarningBanner(tabId);
-      // }
-      //!isWhitelisted && createWarningBanner(tabId);
       updateIcon(isWhitelisted, url, tabId);
     })
     .catch((error) => console.error("Error loading whitelist:", error));
@@ -154,9 +104,7 @@ function getDomain(url) {
 }
 
 function isIPAddress(url) {
-  //console.log(url);
   const urlParts = url.split(".");
-  //console.log("before filter", urlParts);
   return (
     urlParts.filter((part) => {
       if (part >= 0 && part <= 255) {
@@ -168,7 +116,7 @@ function isIPAddress(url) {
 
 function updateIcon(isWhitelisted, url, tabId) {
   let iconPath;
-  console.log(url);
+
   if (url === "new-tab-page") {
     iconPath = "icon/unknownIcon.png";
   } else {
@@ -180,107 +128,17 @@ function updateIcon(isWhitelisted, url, tabId) {
   chrome.action.setIcon({
     path: { 16: iconPath, 48: iconPath, 128: iconPath },
   });
-  //console.log(tabStatusMap[tabId]);
+
   if (!isWhitelisted && tabId > 0) {
-    console.log("Not In List!!");
-    createWarningBanner1(tabId);
+    createWarningBanner(tabId);
   }
 }
 
-// function getDNS(url) {
-//   // Sample URL to be replaced with the user-entered URL
-//   const userEnteredURL = url;
-
-//   // Use a DNS lookup service (replace with your preferred service)
-//   const dnsLookupAPI = "https://dns.google/resolve?name=";
-
-//   // Make a request to the DNS lookup service
-//   fetch(`${dnsLookupAPI}${userEnteredURL}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       // Check if the response is successful
-//       if (data.Status === 0 && data.Answer && data.Answer.length > 0) {
-//         // Extract the IP address from the response
-//         const ipAddress = data.Answer[0].data;
-//         console.log(`IP address of ${userEnteredURL}: ${ipAddress}`);
-//       } else {
-//         console.error(`Failed to retrieve IP address for ${userEnteredURL}`);
-//       }
-//     })
-//     .catch((error) => console.error("Error:", error));
-// }
-
-function createWarningBanner1(tabId) {
+function createWarningBanner(tabId) {
   try {
-    console.log("Send Message!!!", tabId);
-    //chrome.runtime.sendMessage({ action: "createWarningBanner" });
     chrome.tabs.sendMessage(tabId, { action: "createWarningBanner" });
   } catch (error) {
     console.log(error);
-    //chrome.runtime.lastError;
     return null;
   }
 }
-
-// browser.runtime.sendMessage({ hello: "goodbye" }).then(function (response) {
-//   console.log("UUUUUUUUU");
-// });
-
-// function createWarningBanner(tabId) {
-//   try {
-//     console.log(tabId);
-//     chrome.tabs.sendMessage(
-//       tabId,
-//       { action: "createWarningBanner" },
-//       function (response) {
-//         // Handle the response here if needed
-//         console.log("Response from content script:", response);
-//       }
-//     );
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// chrome.runtime.onConnect.addListener(function (port) {
-//   if (port.name === "content-script") {
-//     const tabId = port.sender.tab.id;
-//     tabPorts[tabId] = port;
-
-//     // Listen for messages from content scripts
-//     port.onMessage.addListener(function (msg) {
-//       if (msg.action === "createWarningBanner") {
-//         try {
-//           return createWarningBanner(tabId);
-//         } catch (e) {
-//           console.log(e);
-//           return null;
-//         }
-//       }
-//     });
-
-//     // Remove the port when the tab is closed
-//     port.onDisconnect.addListener(function () {
-//       delete tabPorts[tabId];
-//     });
-//   }
-// });
-
-// function createWarningBanner(tabId) {
-//   const port = tabPorts[tabId];
-//   console.log(port);
-//   if (port) {
-//     port.postMessage({ action: "createWarningBanner" });
-//   }
-// }
-
-// chrome.runtime.onInstalled.addListener(async () => {
-//   for (const cs of chrome.runtime.getManifest().content_scripts) {
-//     for (const tab of await chrome.tabs.query({ url: cs.matches })) {
-//       chrome.scripting.executeScript({
-//         target: { tabId: tab.id },
-//         files: cs.js,
-//       });
-//     }
-//   }
-// });
--- contentScript.js
+++ contentScript.js
@@ -1,45 +1,35 @@
-console.log("Content!!!!!");
 function createWarningBanner() {
   const banner = document.createElement("div");
   banner.innerHTML = `
-    <div id="extension-warning-banner" style="background-color: #FFD700; color: #000; padding: 30px; text-align: center; font-weight: bold;">
-      ⚠️ This site is not in the white list ☠️
+    <div id="extension-warning-banner" >
+      <div id="warning">⚠️ This site is not in the white list ☠️</div>
+      <span id="close-extension-warning">&times; Close this window </span>
     </div>
   `;
 
-  // Apply styles and specific selector
-  banner.style.position = "fixed";
-  banner.style.top = "0";
-  banner.style.left = "0";
-  banner.style.width = "100%";
-  banner.style.zIndex = "9999"; // Set a high z-index
-  banner.style.fontSize = "1.5rem";
-  banner.id = "extension-warning-banner";
-
   document.addEventListener("DOMContentLoaded", function () {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = chrome.runtime.getURL("styles.css");
+    document.head.appendChild(link);
     // Use body as a fallback in case document.body is still null
     const targetNode = document.body || document.documentElement;
     targetNode.prepend(banner);
-  });
 
-  //document.body.prepend(banner);
-  //alert("Not Safe!!");
+    const closeButton = document.getElementById("close-extension-warning");
+    if (closeButton) {
+      closeButton.addEventListener("click", function () {
+        banner.remove();
+      });
+    }
+  });
 }
 
 // Listen for messages from the background script
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log("GGGGGG");
-
   if (request.action === "createWarningBanner") {
     createWarningBanner();
-    //sendResponse({ message: "Warning banner created successfully" });
   }
 });
-
-// const port = chrome.runtime.connect({ name: "content-script" });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   // Send a message to the background script through the port
-//   port.postMessage({ action: "createWarningBanner" });
-// });
